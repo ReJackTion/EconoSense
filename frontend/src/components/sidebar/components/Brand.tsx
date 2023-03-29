cc014c22c94b0dae705d75ab2ce2e116af59f072
ReJackTion <76745858+ReJackTion@users.noreply.github.com>
2023-03-29 21:10:59 +0800

    fix: Change new logo

--- frontend/src/components/sidebar/components/Brand.tsx
+++ frontend/src/components/sidebar/components/Brand.tsx
@@ -2,7 +2,7 @@
 import { Flex, useColorModeValue } from '@chakra-ui/react';
 
 // Custom components
-import { HorizonLogo, SpotifyLogo } from 'components/icons/Icons';
+import EconoSenseLogo from 'components/icons/EconoSenseIcon';
 import { HSeparator } from 'components/separator/Separator';
 
 export function SidebarBrand() {
@@ -11,8 +11,7 @@ export function SidebarBrand() {
 
 	return (
 		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			{/* <SpotifyLogo h='26px' w='175px' my='32px' color={logoColor} /> */}
+			<EconoSenseLogo height='26px' width='175px' my='32px' color={logoColor} />
 			<HSeparator mb='20px' />
 		</Flex>
 	);
